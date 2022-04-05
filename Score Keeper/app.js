05437fbd6e8506bb3f12fa61e14ff2bfba2acbf0
Rtmorais <rt.morais@hotmail.com>
2022-04-05 07:57:09 -0300

    Score Keeper Finished. Adding some styles

--- Score Keeper/app.js
+++ Score Keeper/app.js
@@ -5,10 +5,21 @@ const p1Display = document.querySelector("#p1Display");
 const p2Display = document.querySelector("#p2Display");
 const scoreSelect = document.querySelector("#playTo");
 
-let winningScore = 5;
+function reset() {
+  gameOver = false;
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = p1Score;
+  p2Display.textContent = p2Score;
+  p1Display.classList.remove("winner", "loser");
+  p2Display.classList.remove("winner", "loser");
+}
+
+let winningScore = 3;
 
 scoreSelect.addEventListener("change", function () {
   winningScore = parseInt(this.value);
+  reset();
 });
 
 let gameOver = false;
@@ -19,6 +30,8 @@ p1Button.addEventListener("click", function () {
     p1Score += 1;
     if (p1Score === winningScore) {
       gameOver = true;
+      p1Display.classList.add("winner");
+      p2Display.classList.add("loser");
     }
     p1Display.textContent = p1Score;
   }
@@ -30,15 +43,11 @@ p2Button.addEventListener("click", function () {
     p2Score += 1;
     if (p2Score === winningScore) {
       gameOver = true;
+      p2Display.classList.add("winner");
+      p1Display.classList.add("loser");
     }
     p2Display.textContent = p2Score;
   }
 });
 
-resetButton.addEventListener("click", function () {
-  gameOver = false;
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.textContent = p1Score;
-  p2Display.textContent = p2Score;
-});
+resetButton.addEventListener("click", reset);
