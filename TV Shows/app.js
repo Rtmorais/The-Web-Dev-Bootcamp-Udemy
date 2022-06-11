09945e6936b55da7e17551e6a6ecaeba5f963d92
Rtmorais <rt.morais@hotmail.com>
2022-06-11 16:12:43 -0300

    Tv Shows stylized and done

--- TV Shows/app.js
+++ TV Shows/app.js
@@ -1,21 +1,38 @@
 const form = document.querySelector('#searchForm')
+const resultBlock = document.querySelector('#resultBlock')
 
 form.addEventListener('submit', async function (e) {
   e.preventDefault()
-  const searchTerm = form.elements.query.value
+  removeResult()
 
-  const res = await axios.get(
-    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
-  )
-  makeImages(res.data)
+  const searchTerm = form.elements.query.value
+  const config = { params: { q: searchTerm } }
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config)
+  form.elements.query.value = ''
+
+  makeResult(res.data)
 })
 
-const makeImages = shows => {
+const makeResult = shows => {
   for (let result of shows) {
     if (result.show.image) {
+      const link = document.createElement('a')
+      link.href = result.show.url
+      link.target = '_blank'
+
       const img = document.createElement('img')
-      img.src = result.show.image.medium
-      document.body.append(img)
+      img.src = result.show.image.original
+
+      resultBlock.append(link)
+      link.append(img)
     }
+  }
+}
+
+
+const removeResult = function() {
+  const links = document.querySelectorAll('a')
+  for( let link of links) {
+    link.remove()
   }
 }
